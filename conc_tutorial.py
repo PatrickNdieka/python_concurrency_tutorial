a28e84c22a495cd0aaf92374d47b155586b1fc11
Patrick Onduto <45040687+PatrickNdieka@users.noreply.github.com>
2025-02-11 06:14:53 +0300

    Second commit

--- conc_tutorial.py
+++ conc_tutorial.py
@@ -1,35 +1,37 @@
 import time
 
-from workers.WikiWorker import WikiWorker
-from workers.YFWorker import YahooFinanceWorker
+from multiprocessing import Queue
 
-# def calculate_sum_squares(n):
-#     sum_squares = 0
-#     for i in range(n):
-#         sum_squares += i**2
-#     print(sum_squares)
-    
-# def sleep_a_little(seconds):
-#     time.sleep(seconds)
-    
+from workers.WikiWorker import WikiWorker
+from workers.YFWorker import YahooFinancePriceScheduler
+
 
 def main():
+
+    symbol_queue = Queue()
+
     start_time = time.time()
-    
+
     wikiworker = WikiWorker()
-        
-    current_workers = []
-    
+    yf_price_scheduler_threads = []
+    yf_price_workers = 5
+
+    for i in range(yf_price_workers):
+        yf_price_scheduler = YahooFinancePriceScheduler(
+            input_queue=symbol_queue)
+        yf_price_scheduler_threads.append(yf_price_scheduler)
+
     for symbol in wikiworker.get_sp_500_companies():
-        yf_worker = YahooFinanceWorker(symbol=symbol)
-        
-        current_workers.append(yf_worker)
-    
-    for i in range(len(current_workers)):
-        current_workers[i].join()
-        
-        
+        symbol_queue.put(symbol)
+
+    for i in range(len(yf_price_scheduler_threads)):
+        symbol_queue.put('DONE')
+
+    for i in range(len(yf_price_scheduler_threads)):
+        yf_price_scheduler_threads[i].join()
+
     print('Extracting time took:', round((time.time()-start_time)), 1)
+
 
 if __name__ == "__main__":
     main()
--- workers/YFWorker.py
+++ workers/YFWorker.py
@@ -7,25 +7,42 @@ import time
 from lxml import html
 
 
-class YahooFinanceWorker(threading.Thread):
+class YahooFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
+        super().__init__(**kwargs)
+        self._input_queue = input_queue
+        self.start()
+
+    def run(self):
+        while True:
+            val = self._input_queue.get()
+            if val == 'DONE':
+                break
+
+            yf_finance_worker = YahooFinancePriceWorker(symbol=val)
+            price = yf_finance_worker.get_price()
+            print(price)
+            time.sleep(random.random())
+
+
+class YahooFinancePriceWorker():
     def __init__(self, symbol, **kwargs):
         super().__init__(**kwargs)
         self._symbol = symbol
         base_url = 'https://finance.yahoo.com/quote/'
         self._url = f'{base_url}{self._symbol}'
-        self.start()
-        
-    def run(self):
-        time.sleep(20 * random.random())
+
+    def get_price(self):
         r = requests.get(self._url)
         if r.status_code != 200:
             return
-        
+
         page_content = html.fromstring(r.text)
         try:
-            str_price = page_content.xpath('//*[@id="nimbus-app"]/section/section/section/article/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span')[0].text
+            str_price = page_content.xpath(
+                '//*[@id="nimbus-app"]/section/section/section/article/section[1]/div[2]/div[1]/section/div/section/div[1]/div[1]/span')[0].text
             price = float(str_price.strip().replace(',', ''))
         except Exception as e:
             price = 'N/A'
-        
-        print(price)
+
+        return price
